cba2bf187ee0dc93d2a049a3fc6955315b242be1
Kavin <kavinraja.ganesan@gmail.com>
2024-02-03 17:06:10 +0530

    Bump up resources for webapp

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "webapp" {
   source                           = "./modules/web-app"
 
-  instance_type                    = "t2.micro"
-  root_block_device_volume_size    = 50
-  block_device_volume_size         = 100
-  block_device_iops                = 400
+  instance_type                    = "t3.large"
+  root_block_device_volume_size    = 100
+  block_device_volume_size         = 250
+  block_device_iops                = 500
 
-  hello_world_function_memory_size = 512
+  hello_world_function_memory_size = 1
 }
