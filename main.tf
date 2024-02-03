5fe470130968fa8452821a05597f41f797db8370
Kavin <kavinraja.ganesan@gmail.com>
2024-02-03 17:09:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "webapp" {
   instance_type                    = "t3.large"
   root_block_device_volume_size    = 100
   block_device_volume_size         = 250
-  block_device_iops                = 500
+  block_device_iops                = 600
 
   hello_world_function_memory_size = 1
 }
