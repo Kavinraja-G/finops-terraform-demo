b25613e29169e4b55356be5ebada8c1a5c56cdaf
Kavinraja Ganesan <62742678+Kavinraja-G@users.noreply.github.com>
2024-03-02 23:41:39 +0530

    demo: pr to test finops policies

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "random_uuid" "random_id" {}
 module "webapp" {
   source = "./modules/web-app"
 
-  instance_type                 = "t3.small"
+  instance_type                 = "t3.large"
   root_block_device_volume_size = 100
   block_device_volume_size      = 250
   block_device_iops             = 600
@@ -11,7 +11,7 @@ module "webapp" {
   hello_world_function_memory_size = 128
 
   tags = {
-    ApplicationOwner = "Kavin",
+    # ApplicationOwner = "Kavin",
     FinancialOwner   = "Kavin",
     createdBy        = "Terraform",
     env              = "sandbox"
